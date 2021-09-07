4f55bf50904fa7f4c53d63c32f55febf01b56c58
Agustin Montes <montesagustin99@gmail.com>
2021-09-06 21:34:31 -0300

    Se agrego filtro de binarizacion

--- tp1/js/entregable.js
+++ tp1/js/entregable.js
@@ -10,6 +10,11 @@ let grosor = 1;
 let xInicial = 0;
 let yInicial = 0;
 
+let r = 0;
+let g = 0;
+let b = 0;
+let a = 255;
+
 canvas.addEventListener("mousedown", apretoClick);
 canvas.addEventListener("mouseup", soltoClick);
 canvas.addEventListener("mousemove", movioClick);
@@ -17,6 +22,7 @@ document.getElementById("btnBorrar").addEventListener("click", borrar);
 document.getElementById("btnBorrarTodo").addEventListener("click", borrarTodo);
 document.getElementById("color").addEventListener("input", setColor);
 document.getElementById("grosor").addEventListener("input", setGrosor);
+document.getElementById("bw").addEventListener("click", binarizacion);
 
 function setColor(){
     color = document.getElementById("color").value;
@@ -103,3 +109,62 @@ document.getElementById('file').onchange=function(e){
         }
     }
   }
+
+  function getImgData () {
+    return ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+
+/* function bw() {
+    let imageData = getImgData();
+    let pixels = imageData.data;
+    let numPixels = imageData.width * imageData.height;
+        console.log(imageData)
+        console.log(pixels)
+        console.log(numPixels)
+
+    for ( var i = 0; i < numPixels; i++ ) {
+        var r = pixels[ i * 4 ];
+        var g = pixels[ i * 4 + 1 ];
+        var b = pixels[ i * 4 + 2 ];
+
+        var grey = ( r + g + b ) / 3;
+
+        pixels[ i * 4 ] = grey;  //no le sumo nada porque está en r
+        pixels[ i * 4 + 1 ] = grey; //+1 porque es g
+        pixels[ i * 4 + 2 ] = grey; // +2 porque es b
+    }
+
+    ctx.putImageData( imageData, 0, 0 );
+};*/
+
+function binarizacion() {
+    let imageData = getImgData();
+    drawRect(imageData, r, g, b, a);
+    
+    function drawRect(imageData, r, g, b, a){
+        for (let x = 0; x < imageData.width; x++) {
+            for (let y = 0; y < imageData.height; y++) {
+                setPixel(imageData, x, y, r, g, b, a);
+            }
+        }
+    }
+
+    function setPixel(imageData, x, y, r, g, b, a) {
+        let index = ((x + y * imageData.width) *4);
+
+        r = imageData.data[index + 0];
+        g = imageData.data[index + 1];
+        b = imageData.data[index + 2];
+        a = imageData.data[index + 3];
+
+        let gris = Math.round(((r + g + b) / 3));
+
+        imageData.data[index + 0] = gris;
+        imageData.data[index + 1] = gris;
+        imageData.data[index + 2] = gris;
+
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+};
