42b09764d8c024ea6cd2f1569ea6a68c3b09381d
guidozoppo <1310.guido@gmail.com>
2021-10-16 17:29:37 -0300

    Se comenzo entregable 3

---
+++ tp3/entregable3/js/js.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let juego = document.querySelector("#juego");
+let tuberia = document.querySelector("#tuberia");
+let espacio = document.querySelector("#espacio");
+let character = document.querySelector("#character");
+let puntaje = document.querySelector("#puntaje");
+let cartelHasPerdido = document.querySelector("#cartelHasPerdido");
