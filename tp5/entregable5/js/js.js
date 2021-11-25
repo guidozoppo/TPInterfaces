e08a273f81c289c0a4a1e062dc73ca609156e070
guidozoppo <1310.guido@gmail.com>
2021-11-25 16:57:57 -0300

    Se agrego publicacion con foto y video

--- tp5/entregable5/js/js.js
+++ tp5/entregable5/js/js.js
@@ -1,37 +1,24 @@
 window.addEventListener("DOMContentLoaded", _=>{
 
 document.querySelector("#imgPerfil").addEventListener("click", desplegablePerfil);
+document.querySelector("#like").addEventListener("click", like);
 
-
+    //DESPLEGAR INFORMACION PERFIL
     function desplegablePerfil() {
         document.getElementById("desplegablePerfil").classList.toggle("show");
     }
     
-    // Close the dropdown menu if the user clicks outside of it
-   /*  window.onclick = function(event) {
-      if (!event.target.matches('.dropbtn')) {
-    
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
+    function like(){
+      if(document.querySelector("#like").childNodes[2].innerHTML == "Me gusta"){
+        document.querySelector("#like").childNodes[2].innerHTML = "No me gusta";
+        document.querySelector("#like").childNodes[0].src = "img/like.png";
+      } else {
+        document.querySelector("#like").childNodes[2].innerHTML = "Me gusta";
+        document.querySelector("#like").childNodes[0].src = "img/dislike.png";
       }
-    } */
-
-
-    /* const container = document.querySelector(".container"),
-    privacy = container.querySelector(".post .privacy"),
-    arrowBack = container.querySelector(".audience .arrow-back"); */
-   /*  privacy.addEventListener("click", () => {
-      container.classList.add("active");
-    }); */
-    /* arrowBack.addEventListener("click", () => {
-      container.classList.remove("active");
-    }); */
+      
+    }
+   
 
 
 
