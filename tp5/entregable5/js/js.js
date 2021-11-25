461f9357846b4739f0d0f16c40f3ae1a46053cc5
guidozoppo <1310.guido@gmail.com>
2021-11-25 10:52:31 -0300

    Se inicio crear publicacion

--- tp5/entregable5/js/js.js
+++ tp5/entregable5/js/js.js
@@ -23,7 +23,15 @@ document.querySelector("#imgPerfil").addEventListener("click", desplegablePerfil
     } */
 
 
-
+    /* const container = document.querySelector(".container"),
+    privacy = container.querySelector(".post .privacy"),
+    arrowBack = container.querySelector(".audience .arrow-back"); */
+   /*  privacy.addEventListener("click", () => {
+      container.classList.add("active");
+    }); */
+    /* arrowBack.addEventListener("click", () => {
+      container.classList.remove("active");
+    }); */
 
 
 
