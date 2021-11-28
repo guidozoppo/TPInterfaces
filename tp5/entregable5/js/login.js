78abab3f6d3d53cd8effb857e2fc8a137195bf3c
guidozoppo <1310.guido@gmail.com>
2021-11-28 15:45:41 -0300

    Se adelanto login y register

--- tp5/entregable5/js/login.js
+++ tp5/entregable5/js/login.js
@@ -2,18 +2,33 @@ window.addEventListener("DOMContentLoaded", _=>{
 
     let contador = 0;
     document.getElementById("btn-iniciar-sesion").addEventListener("click", iniciarSesion);
+    document.getElementById("input-usuario").addEventListener("input", checkCampos);
+    document.getElementById("input-contraseña").addEventListener("input", checkCampos);
 
     function iniciarSesion(e){
         e.preventDefault();
         if(contador == 0){
-            console.log("error")
             document.getElementById("msj-error").style.display = "block";
             document.getElementById("input-contraseña").style.border = "2px solid red";
             contador++;
         } else {
-            console.log("ir al home");
-            contador = 0;
+            window.location.href = "home.html";
         }
+    }
+
+    function checkCampos(){
+        if (camposCompletos()) {
+            document.getElementById("btn-iniciar-sesion").disabled = false;
+            document.getElementById("btn-iniciar-sesion").classList.add("botonActivado"); 
+        } else {
+            document.getElementById("btn-iniciar-sesion").disabled = true;
+            document.getElementById("btn-iniciar-sesion").classList.remove("botonActivado"); 
+        }
+    }
+
+    function camposCompletos(){
+        return document.getElementById("input-usuario").value.length > 0 
+                && document.getElementById("input-contraseña").value.length > 0;
     }
 
 })
--- tp5/entregable5/js/register.js
+++ tp5/entregable5/js/register.js
@@ -1,18 +1,38 @@
 window.addEventListener("DOMContentLoaded", _=>{ 
 
-    document.getElementById("input-contraseña").addEventListener("input", checkContraseña);
+    let inputNombre = document.getElementById("input-nombre");
+    let inputApellido = document.getElementById("input-apellido");
+    let inputFechaNac = document.getElementById("input-fechaNac");
+    let inputEmail = document.getElementById("input-email");
+    let inputContraseña = document.getElementById("input-contraseña");
+    
+    inputNombre.addEventListener("input", checkCampos);
+    inputApellido.addEventListener("input", checkCampos);
+    inputFechaNac.addEventListener("input", checkCampos);
+    inputEmail.addEventListener("input", checkCampos);
+    inputContraseña.addEventListener("input", checkContraseña);
+    inputContraseña.addEventListener("input", checkCampos);
+
 
     function checkContraseña(e){
         let length = document.getElementById("input-contraseña").value.length;
         if(length >= 8) {
             document.getElementById("requisitoContraseña").style.color = "#00ff3f";
-            document.getElementById("btn-crear-cuenta").disabled = false;
-            document.getElementById("btn-crear-cuenta").classList.add("asd")
-            document.getElementById("btn-crear-cuenta").classList.add("asdHover")
-
-
         }
     }
     
+    function camposCompletos(){
+        return inputNombre.value.length > 0 && inputApellido.value.length > 0 
+                && inputFechaNac.value.length > 0 && inputEmail.value.length > 0 && inputContraseña.value.length > 0;
+    }
 
+    function checkCampos() {
+        if (camposCompletos() && inputContraseña.value.length >= 8) {
+            document.getElementById("btn-crear-cuenta").disabled = false;
+            document.getElementById("btn-crear-cuenta").classList.add("botonActivado"); 
+        } else {
+            document.getElementById("btn-crear-cuenta").disabled = true;
+            document.getElementById("btn-crear-cuenta").classList.remove("botonActivado"); 
+        }
+    }
 })
